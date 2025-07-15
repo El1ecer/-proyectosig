5ae75bad051c75fd53b96c9a6300237753514cfc
Johan Lozada <johan.lozada5793@utc.edu.ec>
2025-07-15 18:20:51 -0500

    Cambios de zonas de riesgo

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -134,75 +134,55 @@
             </div>
         </div>
     </div>
-
     <!-- Testimonial Start -->
     <!-- Team Start -->
     <div class="container-fluid py-5">
         <div class="container py-5">
             <div class="section-title text-center position-relative mb-5">
-                <h6 class="d-inline-block position-relative text-secondary text-uppercase pb-2">Instructors</h6>
-                <h1 class="display-4">Meet Our Instructors</h1>
+                <h6 class="d-inline-block position-relative text-secondary text-uppercase pb-2">Equipo de trabajo</h6>
+                <h1 class="display-4">Conoce a los desarrolladores</h1>
             </div>
             <div class="owl-carousel team-carousel position-relative" style="padding: 0 30px;">
                 <div class="team-item">
-                    <img class="img-fluid w-100" src="{{ asset('plantilla/img/team-1.jpg') }}" alt="">
+                    <img class="img-fluid w-100" src="{{ asset('plantilla/img/1.png') }}" alt="Justin Álvarez">
                     <div class="bg-light text-center p-4">
-                        <h5 class="mb-3">Instructor Name</h5>
-                        <p class="mb-2">Web Design & Development</p>
+                        <h5 class="mb-3">Justin Álvarez</h5>
+                        <p class="mb-2">Desarrollador Backend</p>
                         <div class="d-flex justify-content-center">
-                            <a class="mx-1 p-1" href="#"><i class="fab fa-twitter"></i></a>
-                            <a class="mx-1 p-1" href="#"><i class="fab fa-facebook-f"></i></a>
+                            <a class="mx-1 p-1" href="#"><i class="fab fa-github"></i></a>
                             <a class="mx-1 p-1" href="#"><i class="fab fa-linkedin-in"></i></a>
                             <a class="mx-1 p-1" href="#"><i class="fab fa-instagram"></i></a>
-                            <a class="mx-1 p-1" href="#"><i class="fab fa-youtube"></i></a>
                         </div>
                     </div>
                 </div>
                 <div class="team-item">
-                    <img class="img-fluid w-100" src="{{ asset('plantilla/img/team-2.jpg') }}" alt="">
+                    <img class="img-fluid w-100" src="{{ asset('plantilla/img/2.png') }}" alt="Johan Lozada">
                     <div class="bg-light text-center p-4">
-                        <h5 class="mb-3">Instructor Name</h5>
-                        <p class="mb-2">Web Design & Development</p>
+                        <h5 class="mb-3">Johan Lozada</h5>
+                        <p class="mb-2">Diseñador Frontend</p>
                         <div class="d-flex justify-content-center">
-                            <a class="mx-1 p-1" href="#"><i class="fab fa-twitter"></i></a>
-                            <a class="mx-1 p-1" href="#"><i class="fab fa-facebook-f"></i></a>
+                            <a class="mx-1 p-1" href="#"><i class="fab fa-github"></i></a>
                             <a class="mx-1 p-1" href="#"><i class="fab fa-linkedin-in"></i></a>
                             <a class="mx-1 p-1" href="#"><i class="fab fa-instagram"></i></a>
-                            <a class="mx-1 p-1" href="#"><i class="fab fa-youtube"></i></a>
                         </div>
                     </div>
                 </div>
                 <div class="team-item">
-                    <img class="img-fluid w-100" src="{{ asset('plantilla/img/team-3.jpg') }}" alt="">
+                    <img class="img-fluid w-100" src="{{ asset('plantilla/img/team-3.jpg') }}" alt="Jorge Medina">
                     <div class="bg-light text-center p-4">
-                        <h5 class="mb-3">Instructor Name</h5>
-                        <p class="mb-2">Web Design & Development</p>
+                        <h5 class="mb-3">Jorge Medina</h5>
+                        <p class="mb-2">Integrador de APIs & Reportes</p>
                         <div class="d-flex justify-content-center">
-                            <a class="mx-1 p-1" href="#"><i class="fab fa-twitter"></i></a>
-                            <a class="mx-1 p-1" href="#"><i class="fab fa-facebook-f"></i></a>
+                            <a class="mx-1 p-1" href="#"><i class="fab fa-github"></i></a>
                             <a class="mx-1 p-1" href="#"><i class="fab fa-linkedin-in"></i></a>
                             <a class="mx-1 p-1" href="#"><i class="fab fa-instagram"></i></a>
-                            <a class="mx-1 p-1" href="#"><i class="fab fa-youtube"></i></a>
-                        </div>
-                    </div>
-                </div>
-                <div class="team-item">
-                    <img class="img-fluid w-100" src="{{ asset('plantilla/img/team-4.jpg') }}" alt="">
-                    <div class="bg-light text-center p-4">
-                        <h5 class="mb-3">Instructor Name</h5>
-                        <p class="mb-2">Web Design & Development</p>
-                        <div class="d-flex justify-content-center">
-                            <a class="mx-1 p-1" href="#"><i class="fab fa-twitter"></i></a>
-                            <a class="mx-1 p-1" href="#"><i class="fab fa-facebook-f"></i></a>
-                            <a class="mx-1 p-1" href="#"><i class="fab fa-linkedin-in"></i></a>
-                            <a class="mx-1 p-1" href="#"><i class="fab fa-instagram"></i></a>
-                            <a class="mx-1 p-1" href="#"><i class="fab fa-youtube"></i></a>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+
     <!-- Team End -->
 
     <!-- Contact Start -->

cd6fbacdc931ad355ce141275895683f12f6461c
Johan Lozada <johan.lozada5793@utc.edu.ec>
2025-07-15 18:00:18 -0500

    Cambios de zonas de riesgos

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,8 +8,6 @@
     </div>
 </div>
 <!-- Header End -->
-HOLAAAAAAAAAAAA
-
 <!-- About Start -->
     <div class="container-fluid py-5">
         <div class="container py-5">
@@ -21,43 +19,20 @@ HOLAAAAAAAAAAAA
                 </div>
                 <div class="col-lg-7">
                     <div class="section-title position-relative mb-4">
-                        <h6 class="d-inline-block position-relative text-secondary text-uppercase pb-2">About Us</h6>
-                        <h1 class="display-4">First Choice For Online Education Anywhere</h1>
+                        <h6 class="d-inline-block position-relative text-secondary text-uppercase pb-2">Sobre nosotros</h6>
+                        <h1 class="display-4">Zonas de riesgo</h1>
                     </div>
-                    <p>Tempor erat elitr at rebum at at clita aliquyam consetetur. Diam dolor diam ipsum et, tempor voluptua sit consetetur sit. Aliquyam diam amet diam et eos sadipscing labore. Clita erat ipsum et lorem et sit, sed stet no labore lorem sit. Sanctus clita duo justo et tempor consetetur takimata eirmod, dolores takimata consetetur invidunt magna dolores aliquyam dolores dolore. Amet erat amet et magna</p>
-                    <div class="row pt-3 mx-0">
-                        <div class="col-3 px-0">
-                            <div class="bg-success text-center p-4">
-                                <h1 class="text-white" data-toggle="counter-up">123</h1>
-                                <h6 class="text-uppercase text-white">Available<span class="d-block">Subjects</span></h6>
-                            </div>
-                        </div>
-                        <div class="col-3 px-0">
-                            <div class="bg-primary text-center p-4">
-                                <h1 class="text-white" data-toggle="counter-up">1234</h1>
-                                <h6 class="text-uppercase text-white">Online<span class="d-block">Courses</span></h6>
-                            </div>
-                        </div>
-                        <div class="col-3 px-0">
-                            <div class="bg-secondary text-center p-4">
-                                <h1 class="text-white" data-toggle="counter-up">123</h1>
-                                <h6 class="text-uppercase text-white">Skilled<span class="d-block">Instructors</span></h6>
-                            </div>
-                        </div>
-                        <div class="col-3 px-0">
-                            <div class="bg-warning text-center p-4">
-                                <h1 class="text-white" data-toggle="counter-up">1234</h1>
-                                <h6 class="text-uppercase text-white">Happy<span class="d-block">Students</span></h6>
-                            </div>
-                        </div>
-                    </div>
+                    <p>
+                        Las <strong>zonas de riesgo</strong> son lugares donde pueden ocurrir peligros que afectan la seguridad de las personas, sus viviendas o el medio ambiente. Estos riesgos pueden ser naturales, como <strong>inundaciones, terremotos, deslizamientos</strong> o <strong>incendios forestales</strong>, o provocados por el ser humano, como <strong>derrames químicos o contaminación</strong>.
+                    </p>
+                    <p>
+                        Conocer estas zonas permite tomar mejores decisiones, prevenir accidentes y salvar vidas. Este sistema ayuda a identificar y visualizar estas áreas en el mapa, según su <strong>nivel de riesgo</strong>, y permite a la comunidad estar informada y preparada en caso de emergencia.
+                    </p>
                 </div>
             </div>
         </div>
     </div>
     <!-- About End -->
-
-
     <!-- Feature Start -->
     <div class="container-fluid bg-image" style="margin: 90px 0;">
         <div class="container">
